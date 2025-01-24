dd62fa26f440540eb5e3089f886c2b4660566951
Chloe Qiao <34826925+qiaochloe@users.noreply.github.com>
2025-01-23 23:50:39 -0500

    Add in-person and remote leaderboard (#214)

--- src/app/(hunt)/teams/page.tsx
+++ src/app/(hunt)/teams/page.tsx
@@ -1,4 +1,4 @@
-import { db } from "~/server/db";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Table,
   TableBody,
@@ -7,87 +7,163 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { db } from "~/server/db";
 import { sql } from "drizzle-orm";
 import { and, asc, desc, eq, lt } from "drizzle-orm/expressions";
 import { teams, guesses } from "~/server/db/schema";
-import { IN_PERSON } from "~/hunt.config";
+import { IN_PERSON, REMOTE } from "~/hunt.config";
 import { FormattedTime } from "~/lib/time";
-
 export const fetchCache = "force-no-store";
 
-export default async function Home() {
-  const teamRows = await db
-    .select({
-      id: teams.id,
-      displayName: teams.displayName,
-      // Exclude finish time if it is after hunt end
-      finishTime: sql<Date | null>`
+type LeaderboardItem = {
+  id: string;
+  displayName: string;
+  finishTime: Date | null;
+  correctGuesses: number;
+  lastCorrectGuessTime: Date;
+};
+
+function Leaderboard({ data }: { data: LeaderboardItem[] }) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>#</TableHead>
+          <TableHead className="w-[20em]">Team Name</TableHead>
+          <TableHead className="w-[10em] text-center">Total Solved</TableHead>
+          <TableHead className="">Finish Time</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((row, index) => (
+          <TableRow className="hover:" key={`${row.id}`}>
+            <TableCell>{index + 1}</TableCell>
+            <TableCell className="w-[20em] break-all">
+              {row.displayName}
+            </TableCell>
+            <TableCell className="text-center">
+              {row.correctGuesses ?? 0}
+            </TableCell>
+            <TableCell>
+              <FormattedTime time={row.finishTime} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+const inPersonTeams: LeaderboardItem[] = await db
+  .select({
+    id: teams.id,
+    displayName: teams.displayName,
+    // Exclude finish time if it is after hunt end
+    finishTime: sql<Date | null>`
         CASE 
           WHEN ${teams.finishTime} > ${IN_PERSON.END_TIME} THEN NULL
           ELSE ${teams.finishTime}
         END`.as("finish_time"),
-      correctGuesses:
-        sql<number>`COUNT(CASE WHEN ${guesses.isCorrect} = true THEN 1 END)`.as(
-          "correct_guesses",
-        ),
-      lastCorrectGuessTime: sql<Date>`MAX(${guesses.submitTime})`.as(
-        "last_correct_guess_time",
+    correctGuesses:
+      sql<number>`COUNT(CASE WHEN ${guesses.isCorrect} = true THEN 1 END)`.as(
+        "correct_guesses",
       ),
-    })
-    .from(teams)
-    // Filter out admin teams and teams who registered after the hunt end
-    .where(
-      and(eq(teams.role, "user"), lt(teams.createTime, IN_PERSON.END_TIME)),
-    )
-    // Get guesses that were submitted before the hunt end
-    // This is used for `correctGuesses` and `lastCorrectGuessTime`
-    .leftJoin(
-      guesses,
-      and(
-        eq(guesses.teamId, teams.id),
-        lt(guesses.submitTime, IN_PERSON.END_TIME),
-      ),
-    )
-    .groupBy(teams.id, teams.displayName, teams.finishTime)
-    .orderBy(
-      asc(teams.finishTime),
-      desc(sql`"correct_guesses"`),
-      asc(sql`"last_correct_guess_time"`),
-    );
+    lastCorrectGuessTime: sql<Date>`MAX(${guesses.submitTime})`.as(
+      "last_correct_guess_time",
+    ),
+  })
+  .from(teams)
+  // Filter out admin teams and teams who registered after the hunt end
+  .where(
+    and(
+      eq(teams.interactionMode, "in-person"),
+      eq(teams.role, "user"),
+      lt(teams.createTime, IN_PERSON.END_TIME),
+    ),
+  )
+  // Get guesses that were submitted before the hunt end
+  // This is used for `correctGuesses` and `lastCorrectGuessTime`
+  .leftJoin(
+    guesses,
+    and(
+      eq(guesses.teamId, teams.id),
+      lt(guesses.submitTime, IN_PERSON.END_TIME),
+    ),
+  )
+  .groupBy(teams.id, teams.displayName, teams.finishTime)
+  .orderBy(
+    asc(teams.finishTime),
+    desc(sql`"correct_guesses"`),
+    asc(sql`"last_correct_guess_time"`),
+  );
 
+const remoteTeams: LeaderboardItem[] = await db
+  .select({
+    id: teams.id,
+    displayName: teams.displayName,
+    // Exclude finish time if it is after hunt end
+    finishTime: sql<Date | null>`
+        CASE 
+          WHEN ${teams.finishTime} > ${REMOTE.END_TIME} THEN NULL
+          ELSE ${teams.finishTime}
+        END`.as("finish_time"),
+    correctGuesses:
+      sql<number>`COUNT(CASE WHEN ${guesses.isCorrect} = true THEN 1 END)`.as(
+        "correct_guesses",
+      ),
+    lastCorrectGuessTime: sql<Date>`MAX(${guesses.submitTime})`.as(
+      "last_correct_guess_time",
+    ),
+  })
+  .from(teams)
+  // Filter out admin teams and teams who registered after the hunt end
+  .where(
+    and(
+      eq(teams.interactionMode, "remote"),
+      eq(teams.role, "user"),
+      lt(teams.createTime, REMOTE.END_TIME),
+    ),
+  )
+  // Get guesses that were submitted before the hunt end
+  // This is used for `correctGuesses` and `lastCorrectGuessTime`
+  .leftJoin(
+    guesses,
+    and(
+      eq(guesses.teamId, teams.id),
+      lt(guesses.submitTime, REMOTE.END_TIME),
+      eq(teams.interactionMode, "remote"),
+    ),
+  )
+  .groupBy(teams.id, teams.displayName, teams.finishTime)
+  .orderBy(
+    asc(teams.finishTime),
+    desc(sql`"correct_guesses"`),
+    asc(sql`"last_correct_guess_time"`),
+  );
+
+export default async function Home() {
   return (
-    <div className="mb-6 flex grow flex-col items-center">
-      <h1 className="mb-2">In-Person Leaderboard!</h1>
-      <div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>#</TableHead>
-              <TableHead className="w-[20em]">Team Name</TableHead>
-              <TableHead className="w-[10em] text-center">
-                Total Solved
-              </TableHead>
-              <TableHead className="">Finish Time</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {teamRows.map((teamRow, index) => (
-              <TableRow className="hover:" key={`${teamRow.id}`}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell className="w-[20em] break-all">
-                  {teamRow.displayName}
-                </TableCell>
-                <TableCell className="text-center">
-                  {teamRow.correctGuesses ?? 0}
-                </TableCell>
-                <TableCell>
-                  <FormattedTime time={teamRow.finishTime} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+    <div className="mb-6 flex grow flex-col items-center p-4">
+      <h1 className="mb-2">Leaderboard!</h1>
+      <Tabs
+        defaultValue="in-person"
+        className="flex max-w-3xl flex-col items-center"
+      >
+        <TabsList className="grid grid-cols-2">
+          <TabsTrigger value="in-person">In-Person</TabsTrigger>
+          <TabsTrigger value="remote">Remote</TabsTrigger>
+        </TabsList>
+        <TabsContent value="in-person">
+          <div className="w-full">
+            <Leaderboard data={inPersonTeams} />
+          </div>
+        </TabsContent>
+        <TabsContent value="remote">
+          <div className="w-full">
+            <Leaderboard data={remoteTeams} />
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
