e32d7b19f9dee085c8babcd98337a5e100082ec4
Chloe Qiao <34826925+qiaochloe@users.noreply.github.com>
2025-03-08 12:06:39 -0500

    Add beads (#104)

---
+++ src/app/(hunt)/puzzle/beads/data.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import BEADS from "./beads.jpg";
+
+/**
+ * The puzzle ID is used to uniquely identify the puzzle in the database.
+ * It should be equal to the name of the folder this file is currently under.
+ * Feel free to make this creative, because the route to the puzzle will be
+ * example.com/puzzle/puzzleId.
+ */
+export const puzzleId = "beads";
+
+/**
+ * The body renders above the guess submission form. Put flavor text, images,
+ * and interactive puzzle components here.
+ */
+export const inPersonBody = (
+  <div className="flex max-w-3xl flex-col items-center space-y-4 text-center">
+    <i className="pb-4">You’ve had me bending double from your curses!</i>
+    <div className="flex justify-center pb-4">
+      <Image src={BEADS} alt="beads" className="w-1/2" />
+    </div>
+  </div>
+);
+
+export const remoteBoxBody = inPersonBody;
+
+export const remoteBody = null;
+
+/**
+ * The `solutionBody` renders in the solution page.
+ * If there are no solutions available, set it null.
+ */
+export const solutionBody = null;
+
+/**
+ * The `copyText` should provide a convenient text representation of the puzzle
+ * that can be copied to the clipboard. Set this to `null` to remove the copy button.
+ */
+export const copyText = null; // Should be composed of the const variables up top
+
+/**
+ * The `partialSolutions` object is used to prompt solutions with significant progress.
+ * Each key is a partial solution, and the value is the prompt to be displayed. Keys must
+ * be in all caps, no spaces.
+ */
+export const partialSolutions: Record<string, string> = {};
+
+/**
+ * The `tasks` object is used for multi-part puzzles. When a certain answer is submitted,
+ * more content will be added to the puzzle body. Keys must be in all caps, no spaces.
+ */
+export const tasks: Record<string, JSX.Element> = {};
