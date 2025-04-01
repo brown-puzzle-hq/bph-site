7555eb838569e8d3dd52fb731d2eef9fbb3dabc7
Chloe Qiao <qiaochloe@gmail.com>
2025-03-31 22:43:31 -0400

    Add sidebar patterns

--- src/app/(hunt)/puzzle/(reality)/layout.tsx
+++ src/app/(hunt)/puzzle/(reality)/layout.tsx
@@ -3,11 +3,11 @@ export default async function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="flex">
-      <div className="hidden min-h-[calc(100vh-56px-32px)] w-1/5 bg-[url('/metas/cerebral.png')] bg-[length:130px_130px] bg-repeat lg:block" />
-      <div className="min-h-[calc(100vh-56px-32px)] w-full bg-[#372825] pt-8">
+      <div className="hidden min-h-[calc(100vh-56px-32px)] w-1/5 bg-[url('/metas/reality.png')] bg-[length:80px_80px] bg-repeat lg:block" />
+      <div className="min-h-[calc(100vh-56px-32px)] w-full bg-[#607D9F] pt-8">
         {children}
       </div>
-      <div className="hidden min-h-[calc(100vh-56px-32px)] w-1/5 bg-[url('/metas/cerebral.png')] bg-[length:130px_130px] bg-repeat lg:block" />
+      <div className="hidden min-h-[calc(100vh-56px-32px)] w-1/5 bg-[url('/metas/reality.png')] bg-[length:80px_80px] bg-repeat lg:block" />
     </div>
   );
 }
