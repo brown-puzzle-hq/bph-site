f7acaa0857f6035be859999b0b2777b76e4909d4
alex-j-wang <151220268+alex-j-wang@users.noreply.github.com>
2025-04-14 15:38:04 -0400

    Add in person solve graph (#260)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import { IN_PERSON, REMOTE } from "./hunt.config";
 
 export default auth(async (req) => {
   // Allow admins to access all pages
@@ -7,26 +8,25 @@ export default auth(async (req) => {
   }
 
   // Protect admin pages from non-admin users
-  if (
-    req.auth?.user?.role !== "admin" &&
-    req.nextUrl.pathname.startsWith("/admin")
-  ) {
+  if (req.nextUrl.pathname.startsWith("/admin")) {
     const newUrl = new URL("./", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 
-  // Protect puzzle pages.
-  // This matches on /puzzle/puzzleId and /puzzle/puzzleId/solution
-  // but not on /puzzle
-  // if (req.nextUrl.pathname.match(/^\/puzzle\/.+/)) {
-  //   if (new Date() < IN_PERSON.START_TIME) {
-  //     const newUrl = new URL("./puzzle", req.nextUrl.origin);
-  //     return Response.redirect(newUrl);
-  //   }
-  // }
+  // Protect wrapup before hunt end
+  if (
+    req.nextUrl.pathname.startsWith("/wrapup") &&
+    new Date() <
+      (req.auth?.user?.interactionMode === "in-person"
+        ? IN_PERSON.END_TIME
+        : REMOTE.END_TIME)
+  ) {
+    const newUrl = new URL("./", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
 });
 
-// WARNING: middleware currently only matches on admin paths
+// WARNING: middleware currently only matches on admin paths and wrapup
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*", "/wrapup/:path*"],
 };
