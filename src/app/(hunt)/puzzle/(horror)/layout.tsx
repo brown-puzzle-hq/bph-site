5c2fd39a5bfec5d770be97a8df038798371aa911
Chloe Qiao <qiaochloe@gmail.com>
2025-04-18 15:45:07 -0400

    Adjust the color of horror

--- src/app/(hunt)/puzzle/(horror)/layout.tsx
+++ src/app/(hunt)/puzzle/(horror)/layout.tsx
@@ -4,7 +4,7 @@ export default async function RootLayout({
   return (
     <div className="flex">
       <div className="hidden min-h-[calc(100vh-56px-32px)] w-1/5 bg-[url('/metas/horror.png')] bg-[length:130px_130px] bg-repeat lg:block" />
-      <div className="min-h-[calc(100vh-56px-32px)] w-full bg-[#372825] pt-8">
+      <div className="min-h-[calc(100vh-56px-32px)] w-full bg-[#332320] pt-8">
         {children}
       </div>
       <div className="hidden min-h-[calc(100vh-56px-32px)] w-1/5 bg-[url('/metas/horror.png')] bg-[length:130px_130px] bg-repeat lg:block" />
