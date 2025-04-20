aa272ecf8f3fa27ba59b3b9269156db81ba97116
Alex Wang <alexander_wang@brown.edu>
2025-04-19 21:37:23 -0400

    Move variables inside function

--- src/app/(hunt)/teams/page.tsx
+++ src/app/(hunt)/teams/page.tsx
@@ -68,74 +68,85 @@ function Leaderboard({ data }: { data: LeaderboardItem[] }) {
   );
 }
 
-const inPersonTeams: LeaderboardItem[] = await db
-  .select({
-    id: teams.id,
-    displayName: teams.displayName,
-    hasBox: teams.hasBox,
-    // Exclude finish time if it is after hunt end
-    finishTime: sql<Date | null>`
-        CASE 
-          WHEN ${teams.finishTime} > ${IN_PERSON.END_TIME} THEN NULL
-          ELSE ${teams.finishTime}
-        END`.as("finish_time"),
-    solves: count(solves).as("solves"),
-    lastSolveTime: max(solves.solveTime).as("last_solve_time"),
-  })
-  .from(teams)
-  // Filter out admin teams and teams who registered after the hunt end
-  .where(
-    and(
-      eq(teams.interactionMode, "in-person"),
-      eq(teams.role, "user"),
-      lt(teams.createTime, IN_PERSON.END_TIME),
-    ),
-  )
-  // Get solves that were submitted before the hunt end
-  // This is used for `solves` and `lastSolveTime`
-  .leftJoin(
-    solves,
-    and(eq(solves.teamId, teams.id), lt(solves.solveTime, IN_PERSON.END_TIME)),
-  )
-  .groupBy(teams.id, teams.displayName, teams.finishTime)
-  .orderBy(asc(sql`finish_time`), desc(sql`solves`), asc(sql`last_solve_time`));
-
-const allRemoteTeams: LeaderboardItem[] = await db
-  .select({
-    id: teams.id,
-    displayName: teams.displayName,
-    hasBox: teams.hasBox,
-    // Exclude finish time if it is after hunt end
-    finishTime: sql<Date | null>`
-        CASE 
-          WHEN ${teams.finishTime} > ${REMOTE.END_TIME} THEN NULL
-          ELSE ${teams.finishTime}
-        END`.as("finish_time"),
-    solves: count(solves).as("solves"),
-    lastSolveTime: max(solves.solveTime).as("last_solve_time"),
-  })
-  .from(teams)
-  // Filter out admin teams and teams who registered after the hunt end
-  .where(
-    and(
-      eq(teams.interactionMode, "remote"),
-      eq(teams.role, "user"),
-      lt(teams.createTime, REMOTE.END_TIME),
-    ),
-  )
-  // Get solves that were submitted before the hunt end
-  // This is used for `solves` and `lastSolveTime`
-  .leftJoin(
-    solves,
-    and(eq(solves.teamId, teams.id), lt(solves.solveTime, REMOTE.END_TIME)),
-  )
-  .groupBy(teams.id, teams.displayName, teams.finishTime, teams.createTime)
-  .orderBy(asc(sql`finish_time`), desc(sql`solves`), asc(sql`last_solve_time`));
-
-const remoteBoxTeams = allRemoteTeams.filter((team) => team.hasBox);
-const remoteTeams = allRemoteTeams.filter((team) => !team.hasBox);
-
 export default async function Home() {
+  const inPersonTeams: LeaderboardItem[] = await db
+    .select({
+      id: teams.id,
+      displayName: teams.displayName,
+      hasBox: teams.hasBox,
+      // Exclude finish time if it is after hunt end
+      finishTime: sql<Date | null>`
+      CASE 
+        WHEN ${teams.finishTime} > ${IN_PERSON.END_TIME} THEN NULL
+        ELSE ${teams.finishTime}
+      END`.as("finish_time"),
+      solves: count(solves).as("solves"),
+      lastSolveTime: max(solves.solveTime).as("last_solve_time"),
+    })
+    .from(teams)
+    // Filter out admin teams and teams who registered after the hunt end
+    .where(
+      and(
+        eq(teams.interactionMode, "in-person"),
+        eq(teams.role, "user"),
+        lt(teams.createTime, IN_PERSON.END_TIME),
+      ),
+    )
+    // Get solves that were submitted before the hunt end
+    // This is used for `solves` and `lastSolveTime`
+    .leftJoin(
+      solves,
+      and(
+        eq(solves.teamId, teams.id),
+        lt(solves.solveTime, IN_PERSON.END_TIME),
+      ),
+    )
+    .groupBy(teams.id, teams.displayName, teams.finishTime)
+    .orderBy(
+      asc(sql`finish_time`),
+      desc(sql`solves`),
+      asc(sql`last_solve_time`),
+    );
+
+  const allRemoteTeams: LeaderboardItem[] = await db
+    .select({
+      id: teams.id,
+      displayName: teams.displayName,
+      hasBox: teams.hasBox,
+      // Exclude finish time if it is after hunt end
+      finishTime: sql<Date | null>`
+      CASE 
+        WHEN ${teams.finishTime} > ${REMOTE.END_TIME} THEN NULL
+        ELSE ${teams.finishTime}
+      END`.as("finish_time"),
+      solves: count(solves).as("solves"),
+      lastSolveTime: max(solves.solveTime).as("last_solve_time"),
+    })
+    .from(teams)
+    // Filter out admin teams and teams who registered after the hunt end
+    .where(
+      and(
+        eq(teams.interactionMode, "remote"),
+        eq(teams.role, "user"),
+        lt(teams.createTime, REMOTE.END_TIME),
+      ),
+    )
+    // Get solves that were submitted before the hunt end
+    // This is used for `solves` and `lastSolveTime`
+    .leftJoin(
+      solves,
+      and(eq(solves.teamId, teams.id), lt(solves.solveTime, REMOTE.END_TIME)),
+    )
+    .groupBy(teams.id, teams.displayName, teams.finishTime, teams.createTime)
+    .orderBy(
+      asc(sql`finish_time`),
+      desc(sql`solves`),
+      asc(sql`last_solve_time`),
+    );
+
+  const remoteBoxTeams = allRemoteTeams.filter((team) => team.hasBox);
+  const remoteTeams = allRemoteTeams.filter((team) => !team.hasBox);
+
   return (
     <div className="mx-auto mb-12 max-w-2xl px-4 pt-6">
       <h1 className="mb-2 text-center">Leaderboard</h1>
