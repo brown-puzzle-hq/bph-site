e9db0b1c19e4978a42eea2344c7b5973313aadd7
Chloe Qiao <qiaochloe@gmail.com>
2025-07-03 10:19:09 -0400

    Testing websocket page

---
+++ src/app/(hunt)/websocket/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { sendToWebsocketServer } from "~/components/websockets/actions";
+
+export default function Page() {
+  return (
+    <div>
+      <button
+        className="mb-4 rounded bg-blue-500 px-4 py-2 text-white"
+        onClick={async () =>
+          await sendToWebsocketServer("team-abc", {
+            type: "Toast",
+            title: "Hi",
+            description: "Hello",
+          })
+        }
+      >
+        Toast
+      </button>
+
+      <button
+        className="mb-4 rounded bg-blue-500 px-4 py-2 text-white"
+        onClick={async () =>
+          await sendToWebsocketServer("team-abc", {
+            type: "UnlockedPuzzle",
+            puzzleId: "example",
+            puzzleName: "Example",
+          })
+        }
+      >
+        Unlock puzzle
+      </button>
+
+      <button
+        className="mb-4 rounded bg-blue-500 px-4 py-2 text-white"
+        onClick={async () =>
+          await sendToWebsocketServer("team-abc", {
+            type: "SolvedPuzzle",
+            puzzleId: "example",
+            puzzleName: "Example",
+          })
+        }
+      >
+        Solve puzzle
+      </button>
+    </div>
+  );
+}
