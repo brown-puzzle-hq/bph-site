612dc1dbfa7b451d482cabf73d68a9af1ff1ce64
Chloe Qiao <qiaochloe@gmail.com>
2025-07-07 15:52:55 -0400

    Minor comments to auth

--- src/server/auth/auth.config.ts
+++ src/server/auth/auth.config.ts
@@ -1,13 +1,17 @@
 import { type NextAuthConfig } from "next-auth";
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- */
 export const authConfig = {
-  session: {
-    strategy: "jwt",
-  },
+  session: { strategy: "jwt" },
+  // Callbacks are async functions that are called
+  // at various points in the authentication flow
+
+  // In order to pass data to the browser,
+  // we persist data in the token during the jwt callback
+  // and pass the data to the browser in the session callback
   callbacks: {
+    // The jwt callback is called whenever a token is created
+    // (ie. at sign-in) or updated (ie. `useSession`).
+    // The returned value is encrypted and stored in a cookie.
     jwt: async ({ token, user, trigger, session }) => {
       if (user) {
         token.id = user.id;
@@ -28,6 +32,10 @@ export const authConfig = {
       }
       return token;
     },
+    // The session callback is called whenever a session
+    // is checked. By default, only a subset of the token is
+    // returned. You need to explicitly forward values to
+    // make it available to the client.
     session: async ({ session, token }) => {
       if (token) {
         session.user = {
@@ -41,6 +49,5 @@ export const authConfig = {
       return session;
     },
   },
-  // adapter: DrizzleAdapter(db),
   providers: [],
 } satisfies NextAuthConfig;
--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -35,9 +35,6 @@ declare module "next-auth" {
   }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- */
 export const { auth, signIn, signOut, handlers } = NextAuth({
   ...authConfig,
   providers: [
@@ -49,28 +46,26 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
       },
       authorize: async (credentials) => {
         try {
-          if (!credentials?.id || !credentials?.password) {
-            return null;
-          }
-
+          // Get id and password
+          if (!credentials?.id || !credentials?.password) return null;
           const { id, password } = await signInSchema.parseAsync(credentials);
 
+          // Get user
           const user = await db.query.teams.findFirst({
             where: eq(teams.id, id.toLowerCase()),
           });
+          if (!user) return null;
 
-          if (user) {
-            const validCredentials = await compare(password, user.password);
-            if (validCredentials) {
-              return {
-                id: user.id,
-                displayName: user.displayName,
-                role: user.role,
-                interactionMode: user.interactionMode,
-              };
-            }
-          }
-          return null;
+          // Check if credentials are valid
+          const validCredentials = await compare(password, user.password);
+          if (!validCredentials) return null;
+
+          return {
+            id: user.id,
+            displayName: user.displayName,
+            role: user.role,
+            interactionMode: user.interactionMode,
+          };
         } catch (error) {
           console.error(error);
           return null;
