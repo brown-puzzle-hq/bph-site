a1d01fa18fde1ab4d05e67e4c7cf5e252a89b7da
alex-j-wang <alex-j-wang@users.noreply.github.com>
2026-02-09 00:07:12 +0000

    Apply formatting changes

--- apps/hunt-site/src/app/admin/websockets/actions.ts
+++ apps/hunt-site/src/app/admin/websockets/actions.ts
@@ -5,7 +5,7 @@ import { assertPermissions } from "~/lib/server";
 
 /** Sends a websocket message as the given team */
 export async function sendWebsocketMessage(msg: SocketMessage) {
-  const { id: teamId} = await assertPermissions({ level: "admin" });
+  const { id: teamId } = await assertPermissions({ level: "admin" });
 
   await sendToWebsocketServer(teamId, msg);
 }
